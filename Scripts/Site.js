73a02a9b5708c527851cbb3a4975a89019b78131
kaleyk <kaleykaukinen@gmail.com>
2021-03-03 12:34:48 -0500

    updated navbar

--- Scripts/Site.js
+++ Scripts/Site.js
@@ -1,7 +1,4 @@
-﻿$(document).ready(function () {
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+﻿    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
     });
-
-});

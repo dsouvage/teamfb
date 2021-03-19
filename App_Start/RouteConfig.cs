232126882f8d21aa6ed889fcec5fda2ac55dce6d
Ryan Montminy <rm16zz@brocku.ca>
2021-03-19 12:15:10 -0400

    Navbar html fix

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace teamfb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
             );
             routes.MapRoute(
                 name: "LoginRoute",

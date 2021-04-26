54a4f8ded62654fd8c1ccef986f4d68eb15ca0d2
Dylan <dsouvage@gmail.com>
2021-04-26 11:18:10 -0400

    add content to client employees orders and products, all dummy and not finished

---
+++ Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace teamfb.Models
+{
+    public class Order
+    {
+        public Order(int BusinessAcountID,string UserName, int Quantity, DateTime date, string type, string desc, int bal)
+        {
+            this.BusinessAcountID = BusinessAcountID;
+            this.UserName = UserName;
+            this.Quantity = Quantity;
+            this.Balance = bal;
+            this.Description = desc;
+            this.ItemType = type;
+            this.DateOfTransaction = date;
+        }
+        public Order()
+        {
+
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID_column { get; set; }
+        [Required]
+        public int BusinessAcountID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public DateTime DateOfTransaction { get; set; }
+        [Required]
+        public string ItemType { get; set;}
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Balance { get; set; }
+
+    }
+}
